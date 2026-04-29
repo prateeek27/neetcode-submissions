3b88d388d010237ea075964acb8ea5674d745162
Prateek Bansal <160769786+prateeek27@users.noreply.github.com>
2026-04-29 22:02:43 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i:nums){
+            if(map.containsKey(i))
+                return true;
+            else
+             map.put(i,i);
+        }
+        return false;
+    }
+}
